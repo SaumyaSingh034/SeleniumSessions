ba7e093d7106bed013feafc2a408941844d710b8
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-28 15:58:18 +0530

    JDBC Connectivity

---
+++ src/test/java/javaSessions/DataBaseConnectivity.java
@@ -0,0 +1,30 @@
+package javaSessions;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBaseConnectivity {
+
+    public WebDriver driver;
+    @Test
+    public void testingDataBaseConnectivity()  {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("url","userName","password");
+            Statement st = con.createStatement();
+            st.executeQuery("query");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
