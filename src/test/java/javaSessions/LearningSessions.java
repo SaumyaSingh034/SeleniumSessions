17d4fb866c55b0a90734dfd47d799e3ecb26b032
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-29 10:53:27 +0530

    Screenshot Learning Session

--- src/test/java/javaSessions/LearningSessions.java
+++ src/test/java/javaSessions/LearningSessions.java
@@ -1,32 +1,48 @@
 package javaSessions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class LearningSessions {
     public WebDriver driver;
 
-    @Test
-    public void learningTest(){
+    @BeforeMethod
+    public void browserSetup()
+    {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+    }
+    @Test
+    public void learningTest(){
+
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,1000)");
 
         Wait<WebDriver> wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(30))
                 .pollingEvery(Duration.ofSeconds(2))
                 .ignoring(NoSuchElementException.class);
-        
 
+    }
 
+    @Test
+    public void learnScreeshot(){
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File src = screenshot.getScreenshotAs(OutputType.FILE);
+        File des = new File("screenshot.png");
+        try {
+            FileUtils.copyFile(src,des);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
