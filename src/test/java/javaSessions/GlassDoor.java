52f38e356a2f3146035e7e67f6365f25eb45a8e7
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-02 19:59:32 +0530

    Final Commit

--- src/test/java/javaSessions/GlassDoor.java
+++ src/test/java/javaSessions/GlassDoor.java
@@ -4,9 +4,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class GlassDoor {
@@ -28,5 +34,17 @@ public class GlassDoor {
         driver.findElement(By.xpath("//button[@class='CloseButton']"));
         String text = driver.findElement(By.xpath("//h1[@class='HeroBannerTNG_center__01NJH py-md-xxl HeroBannerTNG_headerText___YEuP']")).getText();
         System.out.println(text);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        //Explicit Wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("")));
+
+
+        //Fluent
+        Wait<WebDriver> fwait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofSeconds(2))
+                .ignoring(NoSuchElementException.class);
+        fwait.until(ExpectedConditions.visibilityOfElementLocated(By.id("")));
     }
 }
