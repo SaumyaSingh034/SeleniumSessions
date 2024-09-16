f2cb1115118eb23cdefeb8f49ed327b0748373d3
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-16 14:07:29 +0530

    Waits Practice

--- src/test/java/javaSessions/PracticeSession.java
+++ src/test/java/javaSessions/PracticeSession.java
@@ -4,9 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class PracticeSession {
     public WebDriver driver;
@@ -26,5 +33,19 @@ public class PracticeSession {
         WebElement el = driver.findElement(By.cssSelector("tag[@attribute='value']"));
         el.click();
         driver.switchTo().defaultContent();
+    }
+
+    @Test
+    public void explicitWait(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("")));
+
+    }
+
+    @Test
+    public void fluentWait(){
+        Wait<WebDriver> wait1 = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(2)).ignoring(NoSuchElementException.class);
+        wait1.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("")));
     }
 }
