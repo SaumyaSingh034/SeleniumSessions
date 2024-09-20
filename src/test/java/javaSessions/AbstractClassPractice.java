86af71a0f85af17d97aedd575b6c8e6a482fed1c
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-20 22:11:55 +0530

    Abstract Class Implememtation

---
+++ src/test/java/javaSessions/AbstractClassPractice.java
@@ -0,0 +1,14 @@
+package javaSessions;
+
+public abstract class AbstractClassPractice {
+
+    //Contain both abstract and non abstract methods
+
+    public void displayDataForCricket(){
+        System.out.println("Hi I am From Abstract class and you are watching Live Cricket Show");
+    }
+
+    public abstract void cricketTeamCountryName();
+
+    public abstract void cricketTeamJersyColor();
+}
