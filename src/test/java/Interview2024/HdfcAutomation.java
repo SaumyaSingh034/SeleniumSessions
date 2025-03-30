9ecf0fc8f563810744d46c0710137c95dfdbc57a
Saumya <singhsaumyait034@gmail.com>
2025-03-31 00:25:56 +0530

    Hdfc Automation

--- src/test/java/Interview2024/HdfcAutomation.java
+++ src/test/java/Interview2024/HdfcAutomation.java
@@ -15,6 +15,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class HdfcAutomation {
 
@@ -38,11 +41,38 @@ public class HdfcAutomation {
     public void validateLandingPage(){
         WebElement LoginBtn = driver.findElement(By.xpath("//button[@ng-mouseover='mainctrl.login();']")) ;
         System.out.println("Executed SuccessFully amd landed on "+driver.getTitle());
-        driver.findElement(By.xpath("//button[@class='onetrust-close-btn-handler banner-close-button ot-close-icon']")).click();
+        //driver.findElement(By.xpath("//button[@class='onetrust-close-btn-handler banner-close-button ot-close-icon']")).click();
         actions.moveToElement(LoginBtn).perform();
-        //LoginBtn.click();
 
-        //wait.until(ExpectedConditions.alertIsPresent()).dismiss();
+        //Click On Netbanking
+        driver.findElement(By.xpath("(//span[@class='glyphicon glyphicon-menu-right'])[1]")).click();
+        //2 windows active
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> iterator = windows.iterator();
+        String parent = iterator.next();
+        String child = iterator.next();
+
+        //go to child window
+        driver.switchTo().window(child);
+        System.out.println(driver.getTitle());
+
+        //click on know more
+        driver.switchTo().frame("login_page");
+        driver.findElement(By.xpath("((//span[@class='color666'])[2]/following::span)[1]")).click();
+        Set<String> newWindows = driver.getWindowHandles();
+        Iterator<String> newIterator = newWindows.iterator();
+        String childParent = newIterator.next();
+        String childChild = newIterator.next();
+
+        //go to child window
+        driver.switchTo().window(childChild);
+        System.out.println(driver.getTitle());
+
+       List<WebElement> languageList  = driver.findElements(By.xpath("//*[text()='Download the Secure Banking Guide in your preferred language:']/a"));
+       for(WebElement element : languageList){
+           System.out.println("Text :"+element.getText());
+       }
+
 
 
     }
