5861f6a870a53cd8aa6112fedce949fa8aa8d07e
Saumya <singhsaumyait034@gmail.com>
2025-04-24 17:11:49 +0530

    Browser Level Scenarios

--- src/test/java/Interview2024/BrowserLevel.java
+++ src/test/java/Interview2024/BrowserLevel.java
@@ -3,12 +3,18 @@ package Interview2024;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class BrowserLevel implements ITestListener {
     public WebDriver driver;
@@ -32,5 +38,18 @@ public class BrowserLevel implements ITestListener {
     }
     public void onTestSuccess(ITestResult result) {
         System.out.println("✔️ Test passed: " + result.getName());
+    }
+
+    @Test
+    public void waits(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(""))).click();
+
+        Wait fWait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(10)).pollingEvery(Duration.ofSeconds(2))
+                .ignoring(NoSuchElementException.class);
+        fWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("")));
     }
 }
