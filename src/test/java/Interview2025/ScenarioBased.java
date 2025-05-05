187f4d1fbae34bc7d41cdf04bb3b8b6609e5fc88
Saumya <singhsaumyait034@gmail.com>
2025-05-05 21:27:59 +0530

    Scenario based questions

--- src/test/java/Interview2025/ScenarioBased.java
+++ src/test/java/Interview2025/ScenarioBased.java
@@ -1,7 +1,10 @@
 package Interview2025;
 
+import io.cucumber.java.et.Ja;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
@@ -13,5 +16,17 @@ public class ScenarioBased {
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+    }
+
+
+    public void highlightElement(WebDriver driver, WebElement el){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].style.border='3px solid ref'",el);
+    }
+
+    public void flash(WebElement el){
+        String background = el.getCssValue("backgroundColor");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].style.border='3px solid red'",el);
     }
 }
