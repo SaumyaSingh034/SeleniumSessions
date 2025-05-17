6bbb563660e77e0ec82e3a33bd2bafd6fa204dab
Saumya <singhsaumyait034@gmail.com>
2025-05-17 15:49:51 +0530

    Text Decorations

---
+++ src/test/java/Interview2025/ScenarioCoding/HighlightedAndDecorated.java
@@ -0,0 +1,33 @@
+package Interview2025.ScenarioCoding;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class HighlightedAndDecorated {
+    private static WebDriver driver;
+
+    @Test
+    public void testDecoration(){
+       WebElement element =  driver.findElement(By.id("elemenet"));
+        String bgColor = element.getCssValue("background-color");
+        String decoration = element.getCssValue("text-decoration-line");
+        if(!bgColor.equals("transparent") && !bgColor.equals("rgba(0,0,0,0)")){
+            System.out.println("Highlighted");
+        }
+
+        if(decoration.equals("underline")){
+            System.out.println("Underline");
+        }
+    }
+
+    @Test
+    public void shiftDown(){
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.PAGE_DOWN).perform();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollheight);");
+    }
+
+}
